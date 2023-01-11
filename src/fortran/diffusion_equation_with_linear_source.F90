0c5bf78ad38e81559884372af5867c92f0d336df
Chris Bradley <c.bradley@auckland.ac.nz>
2023-01-12 10:54:01 +1300

    Update to code style.

--- src/fortran/diffusion_equation_with_linear_source.F90
+++ src/fortran/diffusion_equation_with_linear_source.F90
@@ -3,10 +3,6 @@ PROGRAM DiffusionEquationWithLinearSource
   USE OpenCMISS
   USE OpenCMISS_Iron
 
-#ifdef WIN32
-  USE IFQWIN
-#endif
-
   IMPLICIT NONE
 
   !-----------------------------------------------------------------------------------------------------------
@@ -18,22 +14,22 @@ PROGRAM DiffusionEquationWithLinearSource
   REAL(CMISSRP), PARAMETER :: WIDTH=2.0_CMISSRP/2.0_CMISSRP
   REAL(CMISSRP), PARAMETER :: LENGTH=3.0_CMISSRP
   
-  INTEGER(CMISSIntg), PARAMETER :: contextUserNumber=1
-  INTEGER(CMISSIntg), PARAMETER :: coordinateSystemUserNumber=1
-  INTEGER(CMISSIntg), PARAMETER :: regionUserNumber=1
-  INTEGER(CMISSIntg), PARAMETER :: basisUserNumber=1
-  INTEGER(CMISSIntg), PARAMETER :: generatedMeshUserNumber=1
-  INTEGER(CMISSIntg), PARAMETER :: meshUserNumber=1
-  INTEGER(CMISSIntg), PARAMETER :: decompositionUserNumber=1
-  INTEGER(CMISSIntg), PARAMETER :: decomposerUserNumber=1
-  INTEGER(CMISSIntg), PARAMETER :: geometricFieldUserNumber=1
-  INTEGER(CMISSIntg), PARAMETER :: equationsSetFieldUserNumber=2
-  INTEGER(CMISSIntg), PARAMETER :: dependentFieldUserNumber=3
-  INTEGER(CMISSIntg), PARAMETER :: materialsFieldUserNumber=4
-  INTEGER(CMISSIntg), PARAMETER :: sourceFieldUserNumber=5
-  INTEGER(CMISSIntg), PARAMETER :: analyticFieldUserNumber=6
-  INTEGER(CMISSIntg), PARAMETER :: equationsSetUserNumber=1
-  INTEGER(CMISSIntg), PARAMETER :: problemUserNumber=1
+  INTEGER(CMISSIntg), PARAMETER :: CONTEXT_USER_NUMBER=1
+  INTEGER(CMISSIntg), PARAMETER :: COORDINATE_SYSTEM_USER_NUMBER=1
+  INTEGER(CMISSIntg), PARAMETER :: REGION_USER_NUMBER=1
+  INTEGER(CMISSIntg), PARAMETER :: BASIS_USER_NUMBER=1
+  INTEGER(CMISSIntg), PARAMETER :: GENERATED_MESH_USER_NUMBER=1
+  INTEGER(CMISSIntg), PARAMETER :: MESH_USER_NUMBER=1
+  INTEGER(CMISSIntg), PARAMETER :: DECOMPOSITION_USER_NUMBER=1
+  INTEGER(CMISSIntg), PARAMETER :: DECOMPOSER_USER_NUMBER=1
+  INTEGER(CMISSIntg), PARAMETER :: GEOMETRIC_FIELD_USER_NUMBER=1
+  INTEGER(CMISSIntg), PARAMETER :: EQUATIONS_SET_FIELD_USER_NUMBER=2
+  INTEGER(CMISSIntg), PARAMETER :: DEPENDENT_FIELD_USER_NUMBER=3
+  INTEGER(CMISSIntg), PARAMETER :: MATERIALS_FIELD_USER_NUMBER=4
+  INTEGER(CMISSIntg), PARAMETER :: SOURCE_FIELD_USER_NUMBER=5
+  INTEGER(CMISSIntg), PARAMETER :: ANALYTIC_FIELD_USER_NUMBER=6
+  INTEGER(CMISSIntg), PARAMETER :: EQUATIONS_SET_USER_NUMBER=1
+  INTEGER(CMISSIntg), PARAMETER :: PROBLEM_USER_NUMBER=1
   
   !Program types
   
@@ -66,18 +62,7 @@ PROGRAM DiffusionEquationWithLinearSource
   INTEGER(CMISSIntg) :: decompositionIndex,equationsSetIndex
   INTEGER(CMISSIntg) :: firstNodeNumber,lastNodeNumber
   INTEGER(CMISSIntg) :: err
-  
-#ifdef WIN32
-  !Initialise QuickWin
-  QUICKWIN_WINDOW_CONFIG%TITLE="General Output" !Window title
-  QUICKWIN_WINDOW_CONFIG%NUMTEXTROWS=-1 !Max possible number of rows
-  QUICKWIN_WINDOW_CONFIG%MODE=QWIN$SCROLLDOWN
-  !Set the window parameters
-  QUICKWIN_STATUS=SETWINDOWCONFIG(QUICKWIN_WINDOW_CONFIG)
-  !If attempt fails set with system estimated values
-  IF(.NOT.QUICKWIN_STATUS) QUICKWIN_STATUS=SETWINDOWCONFIG(QUICKWIN_WINDOW_CONFIG)
-#endif
-
+ 
   !-----------------------------------------------------------------------------------------------------------
   ! PROBLEM CONTROL PANEL
   !-----------------------------------------------------------------------------------------------------------
@@ -89,7 +74,7 @@ PROGRAM DiffusionEquationWithLinearSource
   CALL cmfe_OutputSetOn("DiffusionWithLinearSource",err)
   !Create a context
   CALL cmfe_Context_Initialise(context,err)
-  CALL cmfe_Context_Create(contextUserNumber,context,err)
+  CALL cmfe_Context_Create(CONTEXT_USER_NUMBER,context,err)
   CALL cmfe_ErrorHandlingModeSet(CMFE_ERRORS_TRAP_ERROR,err)
   CALL cmfe_Region_Initialise(worldRegion,err)
   CALL cmfe_Context_WorldRegionGet(context,worldRegion,err)
@@ -119,7 +104,7 @@ PROGRAM DiffusionEquationWithLinearSource
   
   !Start the creation of a new RC coordinate system
   CALL cmfe_CoordinateSystem_Initialise(coordinateSystem,err)
-  CALL cmfe_CoordinateSystem_CreateStart(coordinateSystemUserNumber,context,coordinateSystem,err)
+  CALL cmfe_CoordinateSystem_CreateStart(COORDINATE_SYSTEM_USER_NUMBER,context,coordinateSystem,err)
   IF(numberOfGlobalZElements==0) THEN
     !Set the coordinate system to be 2D
     CALL cmfe_CoordinateSystem_DimensionSet(coordinateSystem,2,err)
@@ -136,7 +121,7 @@ PROGRAM DiffusionEquationWithLinearSource
 
   !Start the creation of the region
   CALL cmfe_Region_Initialise(region,err)
-  CALL cmfe_Region_CreateStart(regionUserNumber,worldRegion,region,err)
+  CALL cmfe_Region_CreateStart(REGION_USER_NUMBER,worldRegion,region,err)
   !Set the regions coordinate system to the 2D RC coordinate system that we have created
   CALL cmfe_Region_CoordinateSystemSet(region,coordinateSystem,err)
   CALL cmfe_Region_LabelSet(region,"diffusion_equation_linear_source",err)
@@ -149,7 +134,7 @@ PROGRAM DiffusionEquationWithLinearSource
   
   !Start the creation of a basis (default is trilinear lagrange)
   CALL cmfe_Basis_Initialise(basis,err)
-  CALL cmfe_Basis_CreateStart(basisUserNumber,context,basis,err)
+  CALL cmfe_Basis_CreateStart(BASIS_USER_NUMBER,context,basis,err)
   IF(numberOfGlobalZElements==0) THEN
     !Set the basis to be a biquadratic Lagrange basis
     CALL cmfe_Basis_NumberOfXiSet(basis,2,err)
@@ -176,7 +161,7 @@ PROGRAM DiffusionEquationWithLinearSource
 
   !Start the creation of a generated mesh in the region
   CALL cmfe_GeneratedMesh_Initialise(generatedMesh,err)
-  CALL cmfe_GeneratedMesh_CreateStart(generatedMeshUserNumber,region,generatedMesh,err)
+  CALL cmfe_GeneratedMesh_CreateStart(GENERATED_MESH_USER_NUMBER,region,generatedMesh,err)
   !Set up a regular x*y*z mesh
   CALL cmfe_GeneratedMesh_TypeSet(generatedMesh,CMFE_GENERATED_MESH_REGULAR_MESH_TYPE,err)
   !Set the default basis
@@ -192,7 +177,7 @@ PROGRAM DiffusionEquationWithLinearSource
   ENDIF
   !Finish the creation of a generated mesh in the region
   CALL cmfe_Mesh_Initialise(Mesh,err)
-  CALL cmfe_GeneratedMesh_CreateFinish(generatedMesh,MeshUserNumber,Mesh,err)
+  CALL cmfe_GeneratedMesh_CreateFinish(generatedMesh,MESH_USER_NUMBER,Mesh,err)
 
   !-----------------------------------------------------------------------------------------------------------
   ! DECOMPOSITION
@@ -200,7 +185,7 @@ PROGRAM DiffusionEquationWithLinearSource
 
   !Create a decomposition
   CALL cmfe_Decomposition_Initialise(decomposition,err)
-  CALL cmfe_Decomposition_CreateStart(decompositionUserNumber,Mesh,decomposition,err)
+  CALL cmfe_Decomposition_CreateStart(DECOMPOSITION_USER_NUMBER,mesh,decomposition,err)
   !Set the decomposition to be a general decomposition with the specified number of domains
   CALL cmfe_Decomposition_TypeSet(decomposition,CMFE_DECOMPOSITION_CALCULATED_TYPE,err)
   !Finish the decomposition
@@ -211,7 +196,7 @@ PROGRAM DiffusionEquationWithLinearSource
   !-----------------------------------------------------------------------------------------------------------
 
   CALL cmfe_Decomposer_Initialise(decomposer,err)
-  CALL cmfe_Decomposer_CreateStart(decomposerUserNumber,region,worldWorkGroup,decomposer,err)
+  CALL cmfe_Decomposer_CreateStart(DECOMPOSER_USER_NUMBER,region,worldWorkGroup,decomposer,err)
   !Add in the decomposition
   CALL cmfe_Decomposer_DecompositionAdd(decomposer,decomposition,decompositionIndex,err)
   !Finish the decomposer
@@ -223,7 +208,7 @@ PROGRAM DiffusionEquationWithLinearSource
   
   !Start to create a default (geometric) field on the region
   CALL cmfe_Field_Initialise(geometricField,err)
-  CALL cmfe_Field_CreateStart(geometricFieldUserNumber,region,geometricField,err)
+  CALL cmfe_Field_CreateStart(GEOMETRIC_FIELD_USER_NUMBER,region,geometricField,err)
   !Set the decomposition to use
   CALL cmfe_Field_DecompositionSet(geometricField,decomposition,err)
   !Set the domain to be used by the field components.
@@ -246,9 +231,9 @@ PROGRAM DiffusionEquationWithLinearSource
   CALL cmfe_EquationsSet_Initialise(equationsSet,err)
   CALL cmfe_Field_Initialise(equationsSetField,err)
   !Set the equations set to be a linear source diffusion problem
-  CALL cmfe_EquationsSet_CreateStart(equationsSetUserNumber,region,geometricField,[CMFE_EQUATIONS_SET_CLASSICAL_FIELD_CLASS, &
-    & CMFE_EQUATIONS_SET_DIFFUSION_EQUATION_TYPE,CMFE_EQUATIONS_SET_GENERALISED_DIFFUSION_SUBTYPE],equationsSetFieldUserNumber, &
-    & equationsSetField,equationsSet,err)  
+  CALL cmfe_EquationsSet_CreateStart(EQUATIONS_SET_USER_NUMBER,region,geometricField,[CMFE_EQUATIONS_SET_CLASSICAL_FIELD_CLASS, &
+    & CMFE_EQUATIONS_SET_DIFFUSION_EQUATION_TYPE,CMFE_EQUATIONS_SET_GENERALISED_DIFFUSION_SUBTYPE], &
+    & EQUATIONS_SET_FIELD_USER_NUMBER,equationsSetField,equationsSet,err)  
   !Finish creating the equations set
   CALL cmfe_EquationsSet_CreateFinish(equationsSet,err)
 
@@ -258,7 +243,7 @@ PROGRAM DiffusionEquationWithLinearSource
 
   !Create the equations set dependent field variables
   CALL cmfe_Field_Initialise(dependentField,err)
-  CALL cmfe_EquationsSet_DependentCreateStart(equationsSet,dependentFieldUserNumber,dependentField,err)
+  CALL cmfe_EquationsSet_DependentCreateStart(equationsSet,DEPENDENT_FIELD_USER_NUMBER,dependentField,err)
   !Finish the equations set dependent field variables
   CALL cmfe_EquationsSet_DependentCreateFinish(equationsSet,err)
 
@@ -268,15 +253,19 @@ PROGRAM DiffusionEquationWithLinearSource
 
   !Create the equations set material field variables
   CALL cmfe_Field_Initialise(materialsField,err)
-  CALL cmfe_EquationsSet_MaterialsCreateStart(equationsSet,MaterialsFieldUserNumber,materialsField,err)
+  CALL cmfe_EquationsSet_MaterialsCreateStart(equationsSet,MATERIALS_FIELD_USER_NUMBER,materialsField,err)
   !Finish the equations set dependent field variables
   CALL cmfe_EquationsSet_MaterialsCreateFinish(equationsSet,err)
   CALL cmfe_Field_ComponentValuesInitialise(materialsField,CMFE_FIELD_U_VARIABLE_TYPE,CMFE_FIELD_VALUES_SET_TYPE, & 
     & 4,-1.0_CMISSRP,err)
   
+  !-----------------------------------------------------------------------------------------------------------
+  ! SOURCE FIELD
+  !-----------------------------------------------------------------------------------------------------------
+
   !Create the equations set source field variables
   CALL cmfe_Field_Initialise(sourceField,err)
-  CALL cmfe_EquationsSet_SourceCreateStart(equationsSet,SourceFieldUserNumber,sourceField,err)
+  CALL cmfe_EquationsSet_SourceCreateStart(equationsSet,SOURCE_FIELD_USER_NUMBER,sourceField,err)
   !Finish the equations set dependent field variables
   CALL cmfe_EquationsSet_SourceCreateFinish(equationsSet,err)
   CALL cmfe_Field_ComponentValuesInitialise(sourceField,CMFE_FIELD_U_VARIABLE_TYPE,CMFE_FIELD_VALUES_SET_TYPE, & 
@@ -289,7 +278,7 @@ PROGRAM DiffusionEquationWithLinearSource
   !Create the equations set analytic field variables
   CALL cmfe_Field_Initialise(analyticField,err)
   CALL cmfe_EquationsSet_AnalyticCreateStart(equationsSet,CMFE_EQUATIONS_SET_LINEAR_SOURCE_DIFFUSION_THREE_DIM_1, & 
-    & analyticFieldUserNumber,analyticField,err)
+    & ANALYTIC_FIELD_USER_NUMBER,analyticField,err)
   !Finish the equations set analytic field variables
   CALL cmfe_EquationsSet_AnalyticCreateFinish(equationsSet,err)
   
@@ -316,7 +305,7 @@ PROGRAM DiffusionEquationWithLinearSource
   
   !Create the problem
   CALL cmfe_Problem_Initialise(problem,err)
-  CALL cmfe_Problem_CreateStart(problemUserNumber,context,[CMFE_PROBLEM_CLASSICAL_FIELD_CLASS, &
+  CALL cmfe_Problem_CreateStart(PROBLEM_USER_NUMBER,context,[CMFE_PROBLEM_CLASSICAL_FIELD_CLASS, &
     & CMFE_PROBLEM_DIFFUSION_EQUATION_TYPE,CMFE_PROBLEM_LINEAR_DIFFUSION_SUBTYPE],problem,err)
   !Finish the creation of a problem.
   CALL cmfe_Problem_CreateFinish(problem,err)
@@ -402,7 +391,6 @@ PROGRAM DiffusionEquationWithLinearSource
   CALL cmfe_Fields_NodesExport(fields,"diffusion_equation_linear_source","FORTRAN",err)
   CALL cmfe_Fields_ElementsExport(fields,"diffusion_equation_linear_source","FORTRAN",err)
   CALL cmfe_Fields_Finalise(fields,err)
-
 
   !Destroy the context
   CALL cmfe_Context_Destroy(context,err)
